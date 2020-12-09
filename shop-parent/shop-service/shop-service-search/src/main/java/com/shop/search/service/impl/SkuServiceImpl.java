53b215a564ca4808525aa95c5454a54a44325d90
XingxingLi2017 <xingxingli2017@gmail.com>
2020-12-09 05:50:49 -0800

    search filtering price range and specs' map

--- shop-parent/shop-service/shop-service-search/src/main/java/com/shop/search/service/impl/SkuServiceImpl.java
+++ shop-parent/shop-service/shop-service-search/src/main/java/com/shop/search/service/impl/SkuServiceImpl.java
@@ -7,6 +7,7 @@ import com.shop.goods.pojo.Sku;
 import com.shop.search.dao.SkuEsMapper;
 import com.shop.search.pojo.SkuInfo;
 import com.shop.search.service.SkuService;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
@@ -58,33 +59,17 @@ public class SkuServiceImpl implements SkuService {
     @Override
     public Map<String, Object> search(Map<String, Object> searchMap) {
 
-        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        // add filters
+        NativeSearchQueryBuilder builder = getNativeSearchQueryBuilder(searchMap);
 
-        // build query by keywords
-        if(searchMap != null && searchMap.size() > 0) {
-            String keywords = (String) searchMap.get("keywords");
-
-            if(!StringUtils.isEmpty(keywords)) {
-                builder.withQuery(
-                        QueryBuilders.queryStringQuery(keywords)
-                                .field("name")
-                );
-            }
-        }
-
-        // add an aggregation operation , query categories containing keywords
-        builder.addAggregation(AggregationBuilders.terms("skuCategoryGroup").field("categoryName").size(50));
-        // get brand name list containing keywords
-        builder.addAggregation(AggregationBuilders.terms("skuBrandGroup").field("brandName").size(50));
-        // aggregate with spec , spec.keyword is used to forbid split words
-        builder.addAggregation(AggregationBuilders.terms("skuSpecGroup").field("spec.keyword").size(100));
+        // add aggregations
+        addAggregations(searchMap, builder);
 
         // execute query
         AggregatedPage<SkuInfo> page = elasticsearchTemplate.queryForPage(builder.build(), SkuInfo.class);
 
         // get aggregation result info
-        StringTerms stringTerms = (StringTerms) page.getAggregation("skuCategoryGroup");
-        List<String> categoryList = getStringsCategoryList(stringTerms);
+        List<String> categoryList = getStringsCategoryList((StringTerms) page.getAggregation("skuCategoryGroup"));
         List<String> brandList = getStringsCategoryList((StringTerms)page.getAggregation("skuBrandGroup"));
         Map<String, Set<String>> specMap = getSpecMap((StringTerms) page.getAggregation("skuSpecGroup"));
 
@@ -103,6 +88,69 @@ public class SkuServiceImpl implements SkuService {
         return ret;
     }
 
+    private void addAggregations(Map<String, Object> searchMap, NativeSearchQueryBuilder builder) {
+
+        if(searchMap == null || StringUtils.isEmpty(searchMap.get("category"))) {
+            // add an aggregation operation , query categories containing keywords
+            builder.addAggregation(AggregationBuilders.terms("skuCategoryGroup").field("categoryName").size(50));
+        }
+
+        if(searchMap == null || StringUtils.isEmpty(searchMap.get("brand"))) {
+            // get brand name list containing keywords
+            builder.addAggregation(AggregationBuilders.terms("skuBrandGroup").field("brandName").size(50));
+        }
+
+        // aggregate with specs , spec.keyword is used to forbid split words
+        builder.addAggregation(AggregationBuilders.terms("skuSpecGroup").field("spec.keyword").size(100));
+
+    }
+
+    private NativeSearchQueryBuilder getNativeSearchQueryBuilder(Map<String, Object> searchMap) {
+
+        NativeSearchQueryBuilder builder = new NativeSearchQueryBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+
+        // build combined query
+        if(searchMap != null && searchMap.size() > 0) {
+
+            // keywords filter
+            if(!StringUtils.isEmpty(searchMap.get("keywords"))) {
+                //builder.withQuery(QueryBuilders.queryStringQuery(keywords).field("name"));
+                boolQueryBuilder.must(QueryBuilders.queryStringQuery((String) searchMap.get("keywords")).field("name"));
+            }
+            if(!StringUtils.isEmpty(searchMap.get("category"))) {
+                boolQueryBuilder.must(QueryBuilders.termQuery("categoryName", searchMap.get("category")));
+            }
+            if(!StringUtils.isEmpty(searchMap.get("brand"))) {
+                boolQueryBuilder.must(QueryBuilders.termQuery("brandName", searchMap.get("brand")));
+            }
+
+            // specs' map filter
+            for(Map.Entry<String, Object> entry : searchMap.entrySet()) {
+                if(entry.getKey().startsWith("spec_")) {
+                    boolQueryBuilder.must(QueryBuilders.termQuery("specMap."+entry.getKey().substring(5)+".keyword", entry.getValue()));
+                }
+            }
+
+            // price filter
+            String price = (String) searchMap.get("price");
+            if(!StringUtils.isEmpty(price)) {
+                price = price.replace("元" , "").replace("以上", "");
+                String[] prices = price.split("-");
+                if(prices != null && prices.length > 0) {
+                    boolQueryBuilder.must(QueryBuilders.rangeQuery("price")
+                            .gt(Integer.parseInt(prices[0])));
+                    if(prices.length > 1) {
+                        boolQueryBuilder.must(QueryBuilders.rangeQuery("price")
+                                .lte(Integer.parseInt(prices[1])));
+                    }
+                }
+            }
+        }
+        builder.withQuery(boolQueryBuilder);
+        return builder;
+    }
+
     private List<String> getStringsCategoryList(StringTerms stringTerms) {
         List<String> categoryList = new ArrayList<>();
         if (stringTerms != null) {
@@ -118,10 +166,14 @@ public class SkuServiceImpl implements SkuService {
         Map<String , Set<String>> specMap = new HashMap<>();
         if(stringTerms != null) {
             for(StringTerms.Bucket bucket : stringTerms.getBuckets()) {
+
+                // bucket : {key1:value1,key2:value2,key3:value3}
                 String keyAsString = bucket.getKeyAsString();
                 Map<String, String> map = JSON.parseObject(keyAsString, Map.class);
 
                 for(Map.Entry<String, String> entry : map.entrySet()) {
+
+                    // use set to remove duplicates
                     Set<String> set = specMap.get(entry.getKey());
                     if(set == null) {
                         set = new HashSet<>();
